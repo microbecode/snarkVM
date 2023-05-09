f46cbd567d17472dfa115b16c2d7a65749c73d47
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2023-05-09 10:07:24 +0300

    local settings for faster run

--- synthesizer/src/vm/verify.rs
+++ synthesizer/src/vm/verify.rs
@@ -249,6 +249,7 @@ mod tests {
          let rng = &mut TestRng::default();
         let vm = crate::vm::test_helpers::sample_vm_with_genesis_block(rng);
 
+
         // Fetch a deployment transaction.
         let deployment_transaction = crate::vm::test_helpers::sample_deployment_transaction(rng);
         // Ensure the transaction verifies.
